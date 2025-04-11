c1f0717db5e580e5ec72a65114c669df3393d02e
Nicolas Gonzalez <ngonzalez2@mail.austral.edu.ar>
2025-04-11 15:14:47 -0300

    Update 1 to both files

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    YEAR = int(input("Ingrese un año: "))
+    B4 = YEAR % 4
+    B400 = YEAR % 400
+    CENTURY = YEAR % 100
+    if B4 == 0 and CENTURY>0:
+        print(f"El año {YEAR} es bisiesto")
+    elif CENTURY == 0 and B400 == 0:
+        print(f"El año {YEAR} es bisiesto")
+    else:
+        print(f"El año {YEAR} no es bisiesto")
+
